250e49803df85876910b27d30a3db891776a39a2
bombini <bombini@admins-iMac.local>
2018-01-11 21:02:49 +0200

    First commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Иполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
